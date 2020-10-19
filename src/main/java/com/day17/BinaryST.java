248c56689a3716ad0f6416fad72dfffc4a9f2cfd
vahbib <singhbibhav084@gmail.com>
2020-10-19 21:32:41 +0530

    Added - UC3: ability to search in binary tree

--- src/main/java/com/day17/BinaryST.java
+++ src/main/java/com/day17/BinaryST.java
@@ -12,4 +12,58 @@ package com.day17;
             right=null;
         }
     }
+public class BinaryST<K extends Comparable> {
 
+    bNode root=null;
+    public void add(K key) {
+        root=addRecursively(root,key);
+    }
+
+    private bNode addRecursively(bNode present, K key) {
+        if(present==null)
+            return new bNode(key);
+        int size=key.compareTo(present.key);
+        if(size==0){
+            return present;
+        }
+        else if(size>0){
+            present.right=addRecursively(present.right,key);
+        }
+        else{
+            present.left=addRecursively(present.left,key);
+        }
+        return present;
+    }
+
+    public int getSize() {
+        return size(root);
+    }
+
+    private int size(bNode current) {
+        return current == null ? 0 : 1 + size(current.left) + size(current.right);
+    }
+
+    public boolean searchKey(K key) {
+        return searchRec(root,key);
+    }
+
+    private boolean searchRec(bNode curr, K key) {
+        boolean present;
+        if(key.compareTo(curr.key)==0){
+            present= true;
+        }
+        else if(key.compareTo(curr.key)>0){
+            if(curr.right==null)
+                present= false;
+            else
+                present=searchRec(curr.right, key);
+        }
+        else{
+            if(curr.left==null)
+                present= false;
+            else
+                present=searchRec(curr.left, key);
+        }
+        return present;
+    }
+}
