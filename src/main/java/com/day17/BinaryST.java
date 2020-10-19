1d5da020b9e2455b0f5de0219a200867e041daf8
vahbib <singhbibhav084@gmail.com>
2020-10-19 21:20:30 +0530

    Added - UC1: ability to create bst by adding 56 and then 30 & 70

--- src/main/java/com/day17/BinaryST.java
+++ src/main/java/com/day17/BinaryST.java
@@ -12,4 +12,35 @@ package com.day17;
             right=null;
         }
     }
+public class BinaryST<K extends Comparable> {
 
+    bNode root=null;
+    public void add(K key) {
+        root=addRecursively(root,key);
+    }
+
+    private bNode addRecursively(bNode present, K key) {
+        if(present==null)
+            return new bNode(key);
+        int size=key.compareTo(present.key);
+        if(size==0){
+            return present;
+        }
+        else if(size>0){
+            present.right=addRecursively(present.right,key);
+        }
+        else{
+            present.left=addRecursively(present.left,key);
+        }
+        return present;
+    }
+
+    public int getSize() {
+        return size(root);
+    }
+
+    private int size(bNode current) {
+        return current == null ? 0 : 1 + size(current.left) + size(current.right);
+    }
+
+}
