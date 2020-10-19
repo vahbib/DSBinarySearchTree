98eeea0a6ae308e1ec557f3e6168c163d1297373
vahbib <singhbibhav084@gmail.com>
2020-10-19 21:25:11 +0530

    Added - UC2: Ability to create the binary tree shown in the figure

--- src/test/java/com/day17/test/BSTTest.java
+++ src/test/java/com/day17/test/BSTTest.java
@@ -1,9 +1,21 @@
 package com.day17.test;
 
+import com.day17.BinaryST;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class BSTTest {
-
+    @Test
+    public void givenNodes_shouldReturnBST() {
+        BinaryST bst = new BinaryST();
+        bst.add(51);
+        bst.add(32);
+        bst.add(75);
+        bst.add(5);
+        bst.add(38);
+        bst.add(89);
+        int size = bst.getSize();
+        Assert.assertEquals(6,size);
+    }
 
 }
